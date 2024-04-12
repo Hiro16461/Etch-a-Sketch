c560afc974714f2c811df0f285c1912cb98dc9ea
Hiro16461 <158548582+Hiro16461@users.noreply.github.com>
2024-04-12 14:48:13 +0900

    Add createNewGrid and drawBGRandom function

--- main.js
+++ main.js
@@ -2,8 +2,28 @@ const gridContainer = document.querySelector('#grid-container');
 const newGridBtn = document.querySelector('.create-new-grid');
 const randomColorBtn = document.querySelector('.random-color');
 
-function createInitialGrids() {
+function createInitialGrid() {
 	for (let i = 0; i < 16 * 16; i++) {
+		const gridItem = document.createElement('div');
+		gridItem.classList.add('grid-item');
+		gridContainer.appendChild(gridItem);
+	}
+}
+
+function createNewGrid() {
+	const newGridItems = +prompt('How many grid do yo want? (ex. 16 x 16)');
+	if (newGridItems > 100) {
+		alert('The number is too big. Please enter the number less than 100');
+	}
+	const gridItems = document.querySelectorAll('.grid-item');
+	Array.from(gridItems).forEach((gridItem) => {
+		gridItem.remove();
+	});
+
+	gridContainer.style.gridTemplateColumns = `repeat(${newGridItems}, 1fr)`;
+	gridContainer.style.gridTemplateRows = `repeat(${newGridItems}, 1fr)`;
+
+	for (let i = 1; i <= newGridItems * newGridItems; i++) {
 		const gridItem = document.createElement('div');
 		gridItem.classList.add('grid-item');
 		gridContainer.appendChild(gridItem);
@@ -29,38 +49,25 @@ function getRandomColor() {
 	return color;
 }
 
-createInitialGrids();
-
-newGridBtn.addEventListener('click', (e) => {
-	const newGridItems = +prompt('How many grid do yo want? (ex. 16 x 16)');
-	if (newGridItems > 100) {
-		alert('The number is too big. Please enter the number less than 100');
-	}
-	const gridItems = document.querySelectorAll('.grid-item');
-	Array.from(gridItems).forEach((gridItem) => {
-		gridItem.remove();
-	});
-
-	gridContainer.style.gridTemplateColumns = `repeat(${newGridItems}, 1fr)`;
-	gridContainer.style.gridTemplateRows = `repeat(${newGridItems}, 1fr)`;
-
-	for (let i = 1; i <= newGridItems * newGridItems; i++) {
-		const gridItem = document.createElement('div');
-		gridItem.classList.add('grid-item');
-		gridContainer.appendChild(gridItem);
-	}
-
-	drawBGRed();
-});
-
-drawBGRed();
-
-randomColorBtn.addEventListener('click', (e) => {
+function drawBGRandom() {
 	const gridItemsDiv = document.querySelectorAll('.grid-item');
 
 	Array.from(gridItemsDiv).forEach((gridItem) => {
 		gridItem.addEventListener('mouseenter', (e) => {
-            e.target.style.backgroundColor = getRandomColor();
+			e.target.style.backgroundColor = getRandomColor();
 		});
 	});
+}
+
+createInitialGrid();
+drawBGRed();
+
+newGridBtn.addEventListener('click', (e) => {
+	createNewGrid();
+	drawBGRed();
+});
+
+
+randomColorBtn.addEventListener('click', (e) => {
+	drawBGRandom();
 });
